cf028dcf2439fbfb88861f5cc44f92e5e29f08f9
v1rasta <v1rasta163@gmail.com>
2022-08-18 12:17:52 +0400

    add Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Привет, как тебя зовут? ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "михаил")
+{
+    Console.WriteLine("Слава белому господину!!!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
